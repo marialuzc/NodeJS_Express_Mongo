baa6fa53c3bf27808363c0ee6bcd685598fa154e
marialuzc <marialuzc@misena.edu.co>
2022-04-14 15:48:32 -0500

    Implementación y prueba de funciones de crear, modificar y borrar (cambiando estado) y listar cursos

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -4,7 +4,87 @@ const ruta = express.Router();
 
 // Endpoint de tipo GET para el recurso Cursos
 ruta.get('/',(req, res) => {
-    res.json('Respuesta a petición GET de cursos funcionando correctamente');
+    let resultado = logic.listarCursosActivos();
+    resultado.then(cursos => {
+        res.json(cursos);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
 });
+
+// Endpoint de tipo POST para el recurso CURSOS
+ruta.post('/', (req, res) => {
+    let resultado = logic.crearCurso(req.body);
+
+    resultado.then(curso => {
+        res.json({
+            curso
+        })
+    }).catch(err => {
+        res.status(400).json({
+            err
+        })
+    })
+});
+
+// Endpoint de tipo PUT para el recurso CURSOS
+ruta.put('/:id', (req, res) => {
+    let resultado = logic.actualizarCurso(req.params.id, req.body);
+    resultado.then(curso => {
+        res.json(curso)
+    }).catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+// Endpoint de tipo DELETE para el recurso CURSOS
+ruta.delete('/:id', (req, res) => {
+    let resultado = logic.desactivarCurso(req.params.id);
+    resultado.then(curso => {
+        res.json(curso);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
+
+// Función asíncrona para crear cursos
+async function crearCurso(body){
+    let curso = new Curso({
+        titulo       : body.titulo,
+        descripcion  : body.descripcion,
+        alumnos  : body.alumnos,
+        calificacion : body.calificacion
+    });
+    return await curso.save();
+}
+
+// Función asíncrona para actualizar cursos
+async function actualizarCurso(id, body){
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set: {
+            titulo: body.titulo,
+            descripcion: body.descripcion,
+            alumnos  : body.alumnos,
+            calificacion : body.calificacion
+        }
+    }, {new: true});
+    return curso;
+}
+
+// Función asíncrona para inactivar cursos
+async function desactivarCurso(id){
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set: {
+            estado: false
+        }
+    }, {new: true});
+    return curso;
+}
+
+// Función asíncrona para listar los cursos activos
+async function listarCursosActivos(){
+    let cursos = await Curso.find({"estado": true});
+    return cursos;
+}
 
 module.exports = ruta;
